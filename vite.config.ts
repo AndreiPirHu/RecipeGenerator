f61dc392c6d2dbe05a259b54871d598861352718
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-01-24 23:03:43 +0100

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/recipeGenerator",
+  base: "recipeGenerator",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Set your custom size limit in kilobytes
+  },
 });
